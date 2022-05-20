631da2a1224ef8f172a7508613f86db33f6cba62
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2022-05-20 14:42:48 -0400

    Add routes for adding, editing items

--- routers/index.router.js
+++ routers/index.router.js
@@ -8,4 +8,38 @@ router.get("/", async (req, res) => {
   return res.render("index", { items });
 });
 
+router.post("/", async (req, res) => {
+  const { name, price, quantity, description } = req.body;
+  const item = new Item({ name, price, quantity, description });
+  await item.save();
+  return res.redirect("/");
+});
+
+router.put("/:itemId", async (req, res) => {
+  const { name, price, quantity, description } = req.body;
+  const item = await Item.findById(req.params.itemId);
+  item.name = name;
+  item.price = price;
+  item.quantity = quantity;
+  item.description = description;
+  await item.save();
+  return res.redirect("/");
+});
+
+router.post("/:itemId/dec", async (req, res) => {
+  const { itemId } = req.params;
+  const item = await Item.findById(itemId);
+  item.quantity--;
+  await item.save();
+  return res.redirect("/");
+});
+
+router.post("/:itemId/inc", async (req, res) => {
+  const { itemId } = req.params;
+  const item = await Item.findById(itemId);
+  item.quantity++;
+  await item.save();
+  return res.redirect("/");
+});
+
 module.exports = router;
