0b9730ff4a4b84dbc5ca54390bd3ca6cb23f6e13
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-05-20 13:13:28 -0400

    Create server.js file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+/* ----------------------- Initializing express server ---------------------- */
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+/* --------------------- Configuring MongoDB Connection --------------------- */
+require("./config/db.config")();
+
+/* ----------------------- Middlewares and View Engine ---------------------- */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+/* --------------------------------- Routes --------------------------------- */
+app.use("/", require("./routers/index.router"));
+
+/* --------------------------- Starting the server -------------------------- */
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
